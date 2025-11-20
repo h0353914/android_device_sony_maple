j4nn/android_device_sony_lilac

lilac: update for twrp-3.7.1_12-0
fix decryption by avoiding cloning of properties on early boot
fix possibility for /system backup in files mode
build kernel from sources instead of prebuilt one

BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_BOOTLOADER_BOARD_NAME := G8441
 
 ### KERNEL
 BOARD_KERNEL_CMDLINE += androidboot.hardware=lilac
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+TARGET_KERNEL_CONFIG := lineage-msm8998-yoshino-lilac_defconfig
 
 ### PARTITIONS
 # See also /proc/partitions on the device

h0353914/android_device_sony_maple

BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_BOOTLOADER_BOARD_NAME := G8141
 
 ### KERNEL
 BOARD_KERNEL_CMDLINE += androidboot.hardware=maple
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+TARGET_KERNEL_CONFIG := lineage-msm8998-yoshino-maple_defconfig
 
 ### PARTITIONS
 # See also /proc/partitions on the device
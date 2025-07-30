d778c44bd6f0ee1028fd4fb56c2cabfff8710f7b
j4nn <531585+j4nn@users.noreply.github.com>
2025-07-31 00:48:19 +0200

    lilac: update for twrp-3.7.1_12-0
    fix decryption by avoiding cloning of properties on early boot
    fix possibility for /system backup in files mode
    build kernel from sources instead of prebuilt one

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_BOOTLOADER_BOARD_NAME := G8141
 
 ### KERNEL
 BOARD_KERNEL_CMDLINE += androidboot.hardware=maple
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+TARGET_KERNEL_CONFIG := lineage-msm8998-yoshino-maple_defconfig
 
 ### PARTITIONS
 # See also /proc/partitions on the device
